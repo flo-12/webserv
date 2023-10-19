2098cc316434400b8c77642698392fcbdf55ddb2
Linus Widmer <lwidmer@c4c4c2.42berlin.de>
2023-10-19 09:16:07 +0200

    refactoring of tcp server

--- tcpserver/tcpserver.c
+++ tcpserver/tcpserver.c
@@ -1,5 +1,6 @@
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,65 +14,84 @@
 #include <netdb.h>
 
 # define SERVER_PORT 18000
+# define BUFSIZE 4000
 
-int main(void)
+int setup_server(int port, int backlog)
 {
-    int                 listenfd, connfd, n;
-    struct sockaddr_in  servaddr;
-    uint8_t             buff[4000];
-    uint8_t             recvline[4000];
+    struct sockaddr_in  server_addr;
+    int                 server_socket;
 
-    // creating a socket
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listenfd < 0)
+    (void)backlog;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
     {
         printf("Error: could not create socket\n");
-        return (0);
+        exit(1);
     }
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
 
-    // setting up the address
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERVER_PORT);
-
-    //binding socket to address
-    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
     {
         printf("Error: could not bind\n");
         return (0);
     }
-    if(listen(listenfd, 10) < 0)
+    if(listen(server_socket, 10) < 0)
     {
         printf("Error: could not listen\n");
         return (0);
     }
+    return (server_socket);
+}
 
-    printf("server listening on port: %i\n", SERVER_PORT);
-    for(;;)
+int accept_new_connection(int server_socket)
+{
+    int                 addr_size = sizeof(struct sockaddr_in);
+    struct sockaddr_in  client_addr;
+    int                 client_socket;
+
+
+    client_socket = accept(server_socket, 
+            (struct sockaddr *)&client_addr, 
+            (socklen_t *)&addr_size);
+    return (client_socket);
+}
+
+void    *handle_connection(int client_socket)
+{
+    char    buffer[BUFSIZE];
+    size_t  bytes_read;
+    int     msgsize = 0;
+    // char    actualpath[PATH_MAX + 1];
+
+    while ((bytes_read = read(client_socket, buffer+msgsize, sizeof(buffer) - msgsize - 1)) > 0)
     {
-        // struct sockaddr_in  addr;
-        // socklen_t           addr_len;
-
-        // wait for a request and print it to terminal
-        connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
-        memset(recvline, 0, 4000);
-        while( (n = read(connfd, recvline, 3999)) > 0 )
-        {
-            printf("\n%s\n", recvline);
-            if (recvline[n-1] == '\n')
+            msgsize += bytes_read;
+            if (msgsize > BUFSIZE - 1 || buffer[msgsize-1] == '\n')
                 break;
-        }
-        memset(recvline, 0, 4000);
-        if(n < 0)
-        {
-            printf("Error: read error\n");
-            return (0);
-        }
+    }
+    buffer[msgsize - 1] = 0;
+    printf("%s\n", buffer);
+    fflush(stdout);
 
-        // send a response
-        snprintf((char *)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello");
-        write(connfd, (char *)buff, strlen((char *)buff));
-        close(connfd);
+    snprintf((char *)buffer, sizeof(buffer), "HTTP/1.0 200 OK\r\n\r\nHello");
+    write(client_socket, (char *)buffer, strlen((char *)buffer));
+    close(client_socket);
+    return (NULL);
+}
+
+int main(void)
+{
+    int                 server_socket;
+    int                 client_socket;
+
+    server_socket = setup_server(SERVER_PORT, 10);
+    printf("server listening on port: %i\n", SERVER_PORT);
+    while(42)
+    {
+        client_socket = accept_new_connection(server_socket);   
+        handle_connection(client_socket);
     }
 }
